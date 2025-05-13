9e8deb7f19dcbfeb2b3e83f91e8d216886a3a835
YoYoLin <yoyo084669@gmail.com>
2025-05-13 15:10:14 +0800

    Delete important_code directory

--- important_code/gelu.h
+++
@@ -1,9 +0,0 @@
-#ifndef GELU_H
-#define GELU_H
-
-
-#include "common.h"
-
-data_t gelu(data_t x);
-
-#endif
--- important_code/relu.h
+++
@@ -1,8 +0,0 @@
-#ifndef RELU_H
-#define RELU_H
-
-#include "common.h"
-
-void relu(data_t input[DIM], data_t output[DIM]);
-
-#endif
